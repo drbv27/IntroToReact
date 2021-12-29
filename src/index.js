4070c4b92435a3b72305f24f243c245976455b45
Diego <drbv27@gmail.com>
2021-12-29 08:49:55 -0500

    Components and properties

--- src/index.js
+++ src/index.js
@@ -11,7 +11,23 @@ import reportWebVitals from "./reportWebVitals";
   document.getElementById('root')
 ); */
 
-const X = () => <p>HOLA MUNDO</p>;
+const Li = ({ children, estado, raza, gordo, edad }) => {
+  console.log(raza, gordo, edad);
+  return (
+    <li>
+      {children} {estado}
+    </li>
+  );
+};
+const X = () => (
+  <ul>
+    <Li estado={"necio"} raza={"snowshoes"} gordo={false} edad={3}>
+      Orion
+    </Li>
+    <Li estado="meloso">Tristan</Li>
+    <Li estado="perezoso">Guido</Li>
+  </ul>
+);
 ReactDOM.render(<X />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
