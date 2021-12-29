bcfd7a226013e34200a7321528f70a9fb2aaf54b
Diego <drbv27@gmail.com>
2021-12-29 09:10:06 -0500

    Component Anatomy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import App from './App';
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-/* ReactDOM.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-); */
+  document.getElementById("root")
+);
 
-const Li = ({ children, estado, raza, gordo, edad }) => {
+/* const Li = ({ children, estado, raza, gordo, edad }) => {
   console.log(raza, gordo, edad);
   return (
     <li>
@@ -28,7 +28,7 @@ const X = () => (
     <Li estado="perezoso">Guido</Li>
   </ul>
 );
-ReactDOM.render(<X />, document.getElementById("root"));
+ReactDOM.render(<X />, document.getElementById("root")); */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
